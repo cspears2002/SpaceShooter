afdbcf0d3c6c3429cf4025987fe9b37efb9cb71b
cspears2002 <cspears2002@gmail.com>
2018-12-01 18:44:23 -0800

    Updated Github for Unity
    Now using version 1.1.1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,18 +20,21 @@ public class PlayerController : MonoBehaviour
 
   private float nextFire;
   private Rigidbody rb;
+  private AudioSource audioSource;
 
-	private void Start()
-	{
+  private void Start()
+  {
       rb = GetComponent<Rigidbody>();
-	}
+      audioSource = GetComponent<AudioSource>();
+  }
 
   void Update()
   {
     if (Input.GetButton("Fire1") && Time.time > nextFire)
       {
-          nextFire = Time.time + fireRate;
-          Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        nextFire = Time.time + fireRate;
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        audioSource.Play();
       }
   }
 
